5a2e2a3ff546172bc7c3a4fcad693902c055139f
Max <maxbretschneider@mailbox.org>
2022-09-12 00:00:23 +0200

    Added basic blob properties

---
+++ Source Files/Blob.cpp
@@ -0,0 +1,15 @@
+#include "Blob.h"
+
+Blob::Blob(std::vector<cv::Point> _contour) {
+
+    contour = _contour;
+
+    boundingRectangle = cv::boundingRectangle(contour);
+
+    centerPosition.x = (boundingRectangle.x + boundingRectangle.x + boundingRectangle.width) / 2;
+    centerPosition.y = (boundingRectangle.y + boundingRectangle.y + boundingRectangle.height) / 2;
+
+    dblDiagonalSize = sqrt(pow(boundingRectangle.width, 2) + pow(boundingRectangle.height, 2));
+
+    dblAspectRatio = (float)boundingRectangle.width / (float)boundingRectangle.height;
+}
