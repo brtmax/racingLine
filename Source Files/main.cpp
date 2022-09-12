5cfa57b2198ce5ff96b99804db12ffe76e4b1c39
Max <maxbretschneider@mailbox.org>
2022-09-12 11:06:34 +0200

    Implemented filters

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -6,6 +6,8 @@
 
 #include "Blob.h"
 
+// I know the code looks horribly right now, I will refactor it soon
+
 const cv::Scalar SCALAR_BLACK = cv::Scalar(0.0, 0.0, 0.0);
 const cv::Scalar SCALAR_WHITE = cv::Scalar(255.0, 255.0, 255.0);
 const cv::Scalar SCALAR_BLUE = cv::Scalar(255.0, 0.0, 0.0);
@@ -24,6 +26,39 @@ int main(void) {
 
         std::cout << "\nError reading video file" << std::endl << std::endl;
         return 0;
+    }
+
+    if (capturedVideo.get(CV_CAP_PROP_FRAME_COUNT) < 2) {
+        std::cout << "\nerror: video file must have at least two frams";
+        return 0;
+    }
+
+    capturedVideo.read(imageFrame1);
+    capturedVideo.read(imageFrame2);
+
+    char keyPressed = 0;
+
+    while(capturedVideo.isOpened() && keyPressed != 27) {
+
+        std::vector<Blob> blobs;
+
+        cv::Mat imageFrame1Copy = imageFrame1.clone();
+        cv::Mat imageFrame2Copy = imageFrame2.clone();
+
+        cv::Mat imageDifference;
+        cv::Mat imageThreshhold;
+
+        cv::cvtColor(imageFrame1Copy, imageFrame1Copy, CV_BGR2GRAY);
+        cv::cvtColor(imageFrame2Copy, imageFrame2Copy, CV_BGR2GRAY);
+
+        cv::GaussianBlur(imageFrame1Copy, imageFrame1Copy, cv::Size(5, 5), 0);
+        cv::GaussianBlur(imageFrame2Copy, imageFrame2Copy, cv::Size(5, 5), 0);
+
+        cv::absdiff(imageFrame1Copy, imageFrame2Copy, imageDifference);
+
+        cv::threshold(imageDifference, imageThreshhold, 30, 255.0, CV_THRESH_BINARY);
+
+
     }
 
 }
