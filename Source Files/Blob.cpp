dc6fe4bf11d66f97a3720810275bed140c351874
Max <maxbretschneider@mailbox.org>
2022-09-15 11:19:46 +0200

    Added rectangle propertie check function

--- Source Files/Blob.cpp
+++ Source Files/Blob.cpp
@@ -12,4 +12,33 @@ Blob::Blob(std::vector<cv::Point> _contour) {
     dblDiagonalSize = sqrt(pow(boundingRectangle.width, 2) + pow(boundingRectangle.height, 2));
 
     dblAspectRatio = (float)boundingRectangle.width / (float)boundingRectangle.height;
+
+    boolean isRealBlob() {
+
+        if (checkRectangleProperties(boundingRectangle) && checkRatioProperties(dblAspectRatio, dblDiagonalSize, )){
+            return true;
+        } 
+        return false;
+    }
+
+    boolean checkRectangleProperties(boundingRectangle) {
+        if (boundingRectangle.width > 15 && boundingRectangle.height > 20 && boundingRectangle.area() > 100) {
+            return true;
+        }
+        return false;
+    }
+
+    boolean checkRatioProperties() {
+
+
+    }
 }
+
+if (possibleBlob.boundingRectangle.area() > 100 &&
+                possibleBlob.dblAspectRatio >= 2.0 &&
+                possibleBlob.dblAspectRatio <= 1.2 &&
+                possibleBlob.boundingRectangle.width > 15 &&
+                possibleBlob.boundingRectangle.height > 20 &&
+                possibleBlob.dblDiagonalSize > 30.0) {
+                    blobs.push_back(possibleBlob);
+                }
