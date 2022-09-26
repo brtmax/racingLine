24fc077e2d2124f4ffaf42f591ad3e05299d5f7e
Max <maxbretschneider@mailbox.org>
2022-09-26 16:05:50 +0200

    Changed trackerrs to cv2.legacy

--- Source Files/object_tracking.py
+++ Source Files/object_tracking.py
@@ -6,6 +6,8 @@ import time
 import cv2
 import sys
 
+from matplotlib.pyplot import box
+
 # construct argument parser and parse arguments
 
 argument_paser = argparse.ArgumentParser()
@@ -18,12 +20,12 @@ args = vars(argument_paser.parse_args())
 OPENCV_OBJECT_TRACKER = {
     "csrt": cv2.TrackerCSRT_create(),
     "kcf": cv2.TrackerKCF_create(),
-    "boosting": cv2.TrackerBoosting_create(),
+    "boosting": cv2.legacy.TrackerBoosting_create(),
     "mil": cv2.TrackerMIL_create(),
-    "tld": cv2.TrackerTLD_create(),
-    "medianflow": cv2.TrackerMedianFlow_create(),
+    "tld": cv2.legacy.TrackerTLD_create(),
+    "medianflow": cv2.legacy.TrackerMedianFlow_create(),
     "goturn": cv2.TrackerGOTURN_create(),
-    "mosse": cv2.TrackerMOSSE_create(),
+    "mosse": cv2.legy.TrackerMOSSE_create(),
 }
 
 tracker = OPENCV_OBJECT_TRACKER[args["tracker"]]()
@@ -42,4 +44,44 @@ if not ok:
     print("Cannot read video file")
     sys.exit()
     
-bbox = cv2.selectROI(frame, False)
+bounding_box = cv2.selectROI(frame, False)
+
+# Initialize the tracker with the first frame and bounding box
+ok = tracker.init(frame, bounding_box)
+
+while True:
+    ok, frame = video.read()
+    if not ok:
+        break
+    # Start the timer
+    timer = cv2.getTickCount()
+    
+    # Update tracker
+    ok, bounding_box = tracker.update(frame)
+    
+    # FPS
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+    # Draw bounding box
+    if ok:
+        # Tracking succes
+        p1 = (int(bounding_box[0]), int(bounding_box[1]))
+        p2 = (int(bounding_box[0] + bounding_box[2]), int(bounding_box[1] + bounding_box[3]))
+        cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+    else :
+        # Tracking Failure
+        cv2.putText(frame, "Tracking failure", (100,80), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.75, (50,170,50),2);
+        
+        # Display tracker on frame
+        cv2.putText(frame, args["tracker"], (100,20), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 0.75, (50,170,50),2);
+        
+        # Display FPS on frame
+        cv2.putText(frame, "FPS: " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.75, (50,170,50),2);
+        
+        # Display result
+        cv2.imshow("Tracking", frame)
+        
+        k = cv2.waitkey(0) & 0xff
+        if k == 27 : break
+        
+        
