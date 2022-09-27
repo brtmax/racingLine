d158834960817511e5abb06e880fb96b6974d410
Max <maxbretschneider@mailbox.org>
2022-09-27 23:23:18 +0200

    Initial commit

---
+++ Source Files/goturn_tracking.cpp
@@ -0,0 +1,12 @@
+// Seeing if it works better using GOTURN with caffeemodel
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/tracking.hpp>
+
+using namespace cv;
+
+#define SSTR( x ) static_cast< std::ostringstream & >( \ (std::ostringstream() << std::dec ) ).str()
+
+int main(int argc, char **argv) {
+    
+}
